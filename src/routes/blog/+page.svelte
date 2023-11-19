fdb1e63edf91a4d51fb83fd95dd27b2359ccd2d0
Mshnwq <hayanx1000@gmail.com>
2023-11-20 02:25:22 +0300

    working routes

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/blog">blog</a>
+</nav>
+
+<h1>Blog page</h1>
+
+<script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
+
+  onMount(() => console.log('Mounted blog page'))
+  onDestroy(() => console.log('Unmounted blog page'))
+</script>
