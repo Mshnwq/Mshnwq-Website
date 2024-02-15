f3b0f87f190aea5b3e5a37b14fb83df9fe8ac696
mshnwq <hayanx1000@gmail.com>
2024-02-15 23:03:36 +0300

    translations added

--- src/helpers/translations.ts
+++ src/helpers/translations.ts
@@ -1,6 +1,7 @@
 import type { Translations, LanguageMeta } from '$src/types/Language';
 
 const translations: Translations = {
+  // English
   en: {
     // Page titles
     'pages.home': 'Home',
@@ -26,15 +27,16 @@ const translations: Translations = {
       'Try selecting more feeds in the dropdown, or broadening your search term',
     'blog.error': 'Oh no, something real bad happened',
   },
+  // French
   fr: {
-    // Page titles
+    // Titres des pages
     'pages.home': 'Accueil',
     'pages.blog': 'Blog',
     'pages.about': 'À propos',
     'pages.contact': 'Contact',
     'pages.projects': 'Projets',
 
-    // Blog page text
+    // Texte de la page Blog
     'blog.sources-menu': 'Sources',
     'blog.sources-all': 'Tout sélectionner',
     'blog.sources-none': 'Tout déselectionner',
@@ -51,231 +53,240 @@ const translations: Translations = {
       'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
     'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
   },
+  // German
   de: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Seitentitel
+    'pages.home': 'Startseite',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.about': 'Über uns',
+    'pages.contact': 'Kontakt',
+    'pages.projects': 'Projekte',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Text auf der Blogseite
+    'blog.sources-menu': 'Quellen',
+    'blog.sources-all': 'Alle auswählen',
+    'blog.sources-none': 'Auswahl aufheben',
+    'blog.read-more': 'Mehr lesen',
+    'blog.time-days-ago': 'Tag(e) zuvor',
+    'blog.time-years-ago': 'Jahr(e) zuvor',
+    'blog.post-hover.read': 'Lesen',
+    'blog.post-hover.from': 'Von',
+    'blog.search.showing': 'Zeige {{count}} Ergebnisse für "{{query}}"',
+    'blog.search.clear': 'Löschen',
+    'blog.results.none': 'Keine Ergebnisse',
+    'blog.results.none-1': 'Es wurden keine Ergebnisse zurückgegeben.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Versuchen Sie, mehr Feeds im Dropdown-Menü auszuwählen oder Ihren Suchbegriff zu erweitern.',
+    'blog.error': 'Oh nein, etwas ist wirklich schiefgelaufen',
   },
+  // Polish
   pl: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Tytuły stron
+    'pages.home': 'Strona główna',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.about': 'O nas',
+    'pages.contact': 'Kontakt',
+    'pages.projects': 'Projekty',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Tekst na stronie Blog
+    'blog.sources-menu': 'Źródła',
+    'blog.sources-all': 'Zaznacz wszystko',
+    'blog.sources-none': 'Wyczyść zaznaczenie',
+    'blog.read-more': 'Czytaj więcej',
+    'blog.time-days-ago': 'dni temu',
+    'blog.time-years-ago': 'lat temu',
+    'blog.post-hover.read': 'Czytaj',
+    'blog.post-hover.from': 'Od',
+    'blog.search.showing': 'Wyświetlanie {{count}} wyników dla "{{query}}"',
+    'blog.search.clear': 'Wyczyść',
+    'blog.results.none': 'Brak wyników',
+    'blog.results.none-1': 'Nie zwrócono żadnych wyników.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Spróbuj wybrać więcej kanałów w rozwijanym menu lub poszerzyć swoje słowo wyszukiwania',
+    'blog.error': 'O nie, coś naprawdę złego się stało',
   },
+  // Dutch
   nl: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Paginatitels
+    'pages.home': 'Home',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
+    'pages.about': 'Over ons',
     'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.projects': 'Projecten',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Tekst op de blogpagina
+    'blog.sources-menu': 'Bronnen',
+    'blog.sources-all': 'Alles selecteren',
+    'blog.sources-none': 'Selectie wissen',
+    'blog.read-more': 'Lees meer',
+    'blog.time-days-ago': 'dagen geleden',
+    'blog.time-years-ago': 'jaar geleden',
+    'blog.post-hover.read': 'Lezen',
+    'blog.post-hover.from': 'Van',
+    'blog.search.showing': 'Toont {{count}} resultaten voor "{{query}}"',
+    'blog.search.clear': 'Wissen',
+    'blog.results.none': 'Geen resultaten',
+    'blog.results.none-1': 'Er zijn geen resultaten geretourneerd.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Probeer meer feeds te selecteren in het dropdownmenu of verbreed uw zoekterm',
+    'blog.error': 'O nee, er is iets heel ergs gebeurd',
   },
+  // Romanian
   ro: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Titlurile paginilor
+    'pages.home': 'Acasă',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
+    'pages.about': 'Despre',
     'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.projects': 'Proiecte',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Text pe pagina Blog
+    'blog.sources-menu': 'Surse',
+    'blog.sources-all': 'Selectează tot',
+    'blog.sources-none': 'Deselectează tot',
+    'blog.read-more': 'Citește mai mult',
+    'blog.time-days-ago': 'zile în urmă',
+    'blog.time-years-ago': 'ani în urmă',
+    'blog.post-hover.read': 'Citește',
+    'blog.post-hover.from': 'De la',
+    'blog.search.showing': 'Se afișează {{count}} rezultate pentru "{{query}}"',
+    'blog.search.clear': 'Șterge',
+    'blog.results.none': 'Niciun rezultat',
+    'blog.results.none-1': 'Nu au fost returnate rezultate.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Încearcă să selectezi mai multe surse în meniul derulant sau să îți extinzi termenul de căutare',
+    'blog.error': 'O, nu, ceva rău s-a întâmplat',
   },
+  // Russian
   ru: {
-    // Page titles
-    'pages.home': 'Accueil',
-    'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    // Заголовки страниц
+    'pages.home': 'Главная',
+    'pages.blog': 'Блог',
+    'pages.about': 'О нас',
+    'pages.contact': 'Контакты',
+    'pages.projects': 'Проекты',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Текст на странице блога
+    'blog.sources-menu': 'Источники',
+    'blog.sources-all': 'Выбрать все',
+    'blog.sources-none': 'Снять выделение',
+    'blog.read-more': 'Читать далее',
+    'blog.time-days-ago': 'дней назад',
+    'blog.time-years-ago': 'лет назад',
+    'blog.post-hover.read': 'Читать',
+    'blog.post-hover.from': 'От',
+    'blog.search.showing': 'Показано {{count}} результатов для "{{query}}"',
+    'blog.search.clear': 'Очистить',
+    'blog.results.none': 'Нет результатов',
+    'blog.results.none-1': 'Результаты не найдены.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Попробуйте выбрать больше источников в выпадающем меню или расширить ваш поисковый запрос',
+    'blog.error': 'О нет, что-то пошло не так',
   },
+  // Spanish
   es: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Títulos de las páginas
+    'pages.home': 'Inicio',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.about': 'Acerca de',
+    'pages.contact': 'Contacto',
+    'pages.projects': 'Proyectos',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
+    // Texto de la página del blog
+    'blog.sources-menu': 'Fuentes',
+    'blog.sources-all': 'Seleccionar todo',
+    'blog.sources-none': 'Limpiar selección',
+    'blog.read-more': 'Leer más',
+    'blog.time-days-ago': 'días atrás',
+    'blog.time-years-ago': 'años atrás',
+    'blog.post-hover.read': 'Leer',
     'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    'blog.search.showing': 'Mostrando {{count}} resultados para "{{query}}"',
+    'blog.search.clear': 'Limpiar',
+    'blog.results.none': 'Sin resultados',
+    'blog.results.none-1': 'No se encontraron resultados.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
-  },
+      'Intente seleccionar más fuentes en el menú desplegable o ampliar su término de búsqueda',
+    'blog.error': 'Oh no, algo realmente malo sucedió',
+  }, 
+  // Italian
   it: {
-    // Page titles
-    'pages.home': 'Accueil',
+    // Titoli delle pagine
+    'pages.home': 'Home',
     'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    'pages.about': 'Chi siamo',
+    'pages.contact': 'Contatti',
+    'pages.projects': 'Progetti',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Testo della pagina del blog
+    'blog.sources-menu': 'Fonti',
+    'blog.sources-all': 'Seleziona tutto',
+    'blog.sources-none': 'Deseleziona tutto',
+    'blog.read-more': 'Leggi di più',
+    'blog.time-days-ago': 'giorni fa',
+    'blog.time-years-ago': 'anni fa',
+    'blog.post-hover.read': 'Leggi',
+    'blog.post-hover.from': 'Da',
+    'blog.search.showing': 'Mostrando {{count}} risultati per "{{query}}"',
+    'blog.search.clear': 'Cancella',
+    'blog.results.none': 'Nessun risultato',
+    'blog.results.none-1': 'Nessun risultato restituito.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Prova a selezionare più feed nel menu a discesa o ad allargare il termine di ricerca',
+    'blog.error': 'Oh no, è successo qualcosa di veramente brutto',
   },
+  // Swedish
   se: {
-    // Page titles
-    'pages.home': 'Accueil',
-    'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    // Sidtitlar
+    'pages.home': 'Hem',
+    'pages.blog': 'Blogg',
+    'pages.about': 'Om oss',
+    'pages.contact': 'Kontakt',
+    'pages.projects': 'Projekt',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Text på bloggsidan
+    'blog.sources-menu': 'Källor',
+    'blog.sources-all': 'Välj alla',
+    'blog.sources-none': 'Rensa markerade',
+    'blog.read-more': 'Läs mer',
+    'blog.time-days-ago': 'dagar sedan',
+    'blog.time-years-ago': 'år sedan',
+    'blog.post-hover.read': 'Läs',
+    'blog.post-hover.from': 'Från',
+    'blog.search.showing': 'Visar {{count}} resultat för "{{query}}"',
+    'blog.search.clear': 'Rensa',
+    'blog.results.none': 'Inga resultat',
+    'blog.results.none-1': 'Inga resultat returnerades.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
+      'Försök att välja fler flöden i rullgardinsmenyn eller bredda din sökterm',
+    'blog.error': 'Åh nej, något riktigt dåligt hände',
   },
+  // Norwegian
   no: {
-    // Page titles
-    'pages.home': 'Accueil',
-    'pages.blog': 'Blog',
-    'pages.about': 'À propos',
-    'pages.contact': 'Contact',
-    'pages.projects': 'Projets',
+    // Sidetitler
+    'pages.home': 'Hjem',
+    'pages.blog': 'Blogg',
+    'pages.about': 'Om oss',
+    'pages.contact': 'Kontakt',
+    'pages.projects': 'Prosjekter',
 
-    // Blog page text
-    'blog.sources-menu': 'Sources',
-    'blog.sources-all': 'Tout sélectionner',
-    'blog.sources-none': 'Tout déselectionner',
-    'blog.read-more': 'Lire la suite',
-    'blog.time-days-ago': 'jours passés',
-    'blog.time-years-ago': 'ans passés',
-    'blog.post-hover.read': 'Lire',
-    'blog.post-hover.from': 'De',
-    'blog.search.showing': 'Affichage de {{count}} résultats pour "{{query}}"',
-    'blog.search.clear': 'Effacer',
-    'blog.results.none': 'Aucun résultat',
-    'blog.results.none-1': "Aucun résultat n'a été trouvé.",
+    // Tekst på bloggsiden
+    'blog.sources-menu': 'Kilder',
+    'blog.sources-all': 'Velg alle',
+    'blog.sources-none': 'Fjern merking',
+    'blog.read-more': 'Les mer',
+    'blog.time-days-ago': 'dager siden',
+    'blog.time-years-ago': 'år siden',
+    'blog.post-hover.read': 'Les',
+    'blog.post-hover.from': 'Fra',
+    'blog.search.showing': 'Viser {{count}} resultater for "{{query}}"',
+    'blog.search.clear': 'Tøm',
+    'blog.results.none': 'Ingen resultater',
+    'blog.results.none-1': 'Ingen resultater ble returnert.',
     'blog.results.none-2':
-      'Essayez de sélectionner plus de flux dans le menu déroulant, ou élargissez votre terme de recherche',
-    'blog.error': 'Oh non, quelque chose de vraiment mauvais est arrivé',
-  },
+      'Prøv å velge flere kilder i rullegardinmenyen, eller utvid søket ditt',
+    'blog.error': 'Å nei, noe virkelig dårlig skjedde',
+  }, 
 };
 
 export const supportedLanguages: LanguageMeta[] = [
@@ -298,19 +309,19 @@ export const supportedLanguages: LanguageMeta[] = [
     text: translations.de,
   },
   {
-    name: 'Russian',
+    name: 'Русский',
     flag: '🇷🇺',
     code: 'ru',
     text: translations.ru,
   },
   {
-    name: 'Romanian',
+    name: 'Română',
     flag: '🇷🇴',
     code: 'ro',
     text: translations.ro,
   },
   {
-    name: 'Espanol',
+    name: 'Español',
     flag: '🇪🇸',
     code: 'es',
     text: translations.es,
@@ -322,25 +333,25 @@ export const supportedLanguages: LanguageMeta[] = [
     text: translations.it,
   },
   {
-    name: 'Swedish',
+    name: 'Svenska',
     flag: '🇸🇪',
     code: 'se',
     text: translations.se,
   },
   {
-    name: 'Norweigan',
+    name: 'Norsk',
     flag: '🇳🇴',
     code: 'no',
     text: translations.no,
   },
   {
-    name: 'Netherlands',
+    name: 'Nederlands',
     flag: '🇳🇱',
     code: 'nl',
     text: translations.nl,
   },
   {
-    name: 'Poland',
+    name: 'Polski',
     flag: '🇵🇱',
     code: 'pl',
     text: translations.pl,
